69d79d538bca712f07afae2021b77dbed14c9e7f
h-github <hmohammadi@hotmail.ca>
2020-07-25 00:44:48 -0700

    first commit-used ef

---
+++ AdventureOrderApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using AdventureOrderApp.Data;
+using System;
+using System.Linq;
+
+namespace AdventureOrderApp
+{
+  class Program
+  {
+    private static AdventureWorksContext context = new AdventureWorksContext();
+
+    static void Main(string[] args)
+    {
+
+      var someOrders = context.SalesOrderHeader.Where(ord => ord.SubTotal > 100000m).ToList();
+      Console.WriteLine(someOrders.Count);
+      foreach (var order in someOrders)
+      {
+        Console.WriteLine(order.SalesOrderId);
+      }
+      Console.ReadLine();
+    }
+  }
+}
