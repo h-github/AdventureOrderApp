492b688de8b59001cb01d4cbf78cac7c5456a3f2
h-github <hmohammadi@hotmail.ca>
2020-07-25 13:30:28 -0700

    Initial EF migration added to the Data project

---
+++ AdventureOrderApp.Data/Migrations/20200725202129_initial.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdventureOrderApp.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AWBuildVersion");
+
+            migrationBuilder.DropTable(
+                name: "DatabaseLog");
+
+            migrationBuilder.DropTable(
+                name: "ErrorLog");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeDepartmentHistory",
+                schema: "HumanResources");
+
+            migrationBuilder.DropTable(
+                name: "EmployeePayHistory",
+                schema: "HumanResources");
+
+            migrationBuilder.DropTable(
+                name: "JobCandidate",
+                schema: "HumanResources");
+
+            migrationBuilder.DropTable(
+                name: "BusinessEntityAddress",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "BusinessEntityContact",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "EmailAddress",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "Password",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "PersonPhone",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "BillOfMaterials",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductCostHistory",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductInventory",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductListPriceHistory",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductModelIllustration",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductModelProductDescriptionCulture",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductProductPhoto",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductReview",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "TransactionHistory",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "TransactionHistoryArchive",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "WorkOrderRouting",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductVendor",
+                schema: "Purchasing");
+
+            migrationBuilder.DropTable(
+                name: "PurchaseOrderDetail",
+                schema: "Purchasing");
+
+            migrationBuilder.DropTable(
+                name: "CountryRegionCurrency",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "PersonCreditCard",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "SalesOrderDetail",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "SalesOrderHeaderSalesReason",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "SalesPersonQuotaHistory",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "SalesTaxRate",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "SalesTerritoryHistory",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingCartItem",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Department",
+                schema: "HumanResources");
+
+            migrationBuilder.DropTable(
+                name: "Shift",
+                schema: "HumanResources");
+
+            migrationBuilder.DropTable(
+                name: "AddressType",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "ContactType",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "PhoneNumberType",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "Illustration",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "Culture",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductDescription",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductPhoto",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "Location",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "WorkOrder",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "PurchaseOrderHeader",
+                schema: "Purchasing");
+
+            migrationBuilder.DropTable(
+                name: "SpecialOfferProduct",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "SalesOrderHeader",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "SalesReason",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "ScrapReason",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "Vendor",
+                schema: "Purchasing");
+
+            migrationBuilder.DropTable(
+                name: "Product",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "SpecialOffer",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Address",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "CreditCard",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "CurrencyRate",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Customer",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "ShipMethod",
+                schema: "Purchasing");
+
+            migrationBuilder.DropTable(
+                name: "ProductModel",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "ProductSubcategory",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "UnitMeasure",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "StateProvince",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "Currency",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Store",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategory",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "SalesPerson",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Employee",
+                schema: "HumanResources");
+
+            migrationBuilder.DropTable(
+                name: "SalesTerritory",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Person",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "CountryRegion",
+                schema: "Person");
+
+            migrationBuilder.DropTable(
+                name: "BusinessEntity",
+                schema: "Person");
+        }
+    }
+}
